da22b6f98fa6143c3a58df4caa01fff5793de47c
ddoniddoni <psdkei@naver.com>
2020-04-15 17:50:38 +0900

    JS BASIC

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const calculator = {
-  plus: (a, b) => {
-    return a + b;
-  },
-  minus: (a, b) => {
-    return a - b;
-  },
-  pow: (a, b) => {
-    return a * b;
-  },
-  div: (a, b) => {
-    return a / b;
-  },
-};
-
-const plus = calculator.plus(5, 5);
-const minus = calculator.minus(5, 5);
-const pow = calculator.pow(5, 5);
-const div = calculator.div(5, 5);
-console.log(plus, minus, pow, div);
+const title = document.getElementById("title");
+title.innerHTML = " HI~ FIROM JS";
