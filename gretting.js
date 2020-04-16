8b0734032500c15ffaed91b4cb7d57368c846fdb
ddoniddoni <psdkei@naver.com>
2020-04-16 17:22:29 +0900

    use LocalStorage

--- gretting.js
+++ gretting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+const saveName = (text) => {
+  localStorage.setItem(USER_LS, text);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+};
+
+const askForName = () => {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+};
+
 const paintGreeting = (text) => {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +30,7 @@ const paintGreeting = (text) => {
 const loadName = () => {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
