c7daaf0d7da5a5227f518ae0807f2348e96b65a1
ddoniddoni <psdkei@naver.com>
2020-04-17 17:29:04 +0900

    use WeatherApi

--- weather.js
+++ weather.js
@@ -1,5 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "d8395d193557f6d8cb03d38b69442b17";
 const COORDS = "coords";
+
+const getWeather = (lat, lng) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+};
 
 const saveCoords = (coordsObj) => {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +29,7 @@ const handleGeoSucces = (position) => {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 };
 const handleGeoError = () => {
   console.log("Cant access geo Location");
@@ -23,10 +40,12 @@ const askForCoords = () => {
 };
 
 const loadCoords = () => {
-  const loadCoords = localStorage.getItem(COORDS);
-  if (loadCoords === null) {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 };
 
